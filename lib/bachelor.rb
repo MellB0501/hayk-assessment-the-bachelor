fe6e864754ada4d05af6829a16ea5003072c31eb
Mell Basham <mellbasham@gmail.com>
2021-02-03 13:44:19 -0700

    commit tree 7cd263673ff8fa0e67a996fb2a9820da6bdbfa33

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,30 @@
+# require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_winner = []
+  data.each do |season|
+    season = "Winner"["name"]
+  season_winner << season
+  end
+  season_winner
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+def get_contestant_name(data, season)
+  contestant_name = seasons.find(data, season) do |contestant|
+    contestant = contestant_name[:name]
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestant_hometown = seasons.find(data, hometown) do |contestant|
+    contestant = contestant_hometown[:hometown]
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant_occupation = seasons.find(data, hometown) do |contestant|
+    contestant = contestant_occupation[:occupation]
+  end
 end
 
 def get_average_age_for_season(data, season)
